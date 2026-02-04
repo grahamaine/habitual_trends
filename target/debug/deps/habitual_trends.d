44137c85623c34629100e8fa8ffa61b2a6983133
Graham Byagaba <ainegb05@gmail.com>
2026-02-04 09:40:26 +0400

    chore: strictly remove build artifacts from version control

--- target/debug/deps/habitual_trends.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Lenovo\Desktop\habitual_trends\target\debug\deps\habitual_trends.d: src\main.rs
-
-C:\Users\Lenovo\Desktop\habitual_trends\target\debug\deps\habitual_trends.exe: src\main.rs
-
-src\main.rs:
--- target/debug/habitual_trends.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Lenovo\Desktop\habitual_trends\target\debug\habitual_trends.exe: C:\Users\Lenovo\Desktop\habitual_trends\src\main.rs
